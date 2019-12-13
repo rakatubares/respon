3858d0719f3aec63e598a4cd00ad61ae26d78d3c
rakatubares <rakatubares@gmail.com>
2019-12-13 14:33:55 +0700

    handling second request

--- controller/app/manifest.py
+++ controller/app/manifest.py
@@ -14,55 +14,58 @@ class Manifest(object):
 	def __init__(self):
 		super(Manifest, self).__init__()
 		self.url = 'http://manif-in.customs.go.id/beacukai-manifes'
+		self.driver = ''
 		self.responses = []
 		
 	def openPage(self):
-		driver = Login(self.url)
-		driver = driver.login()
-		return driver
+		self.driver = Login(self.url)
+		self.driver = self.driver.login()
+		return self.driver
 
 	def openMenu(self):
-		driver = self.openPage()
+		self.driver = self.openPage()
 
 		print('Open menu..')
-		menuUtility = driver.find_element_by_css_selector('.z-menu:nth-child(4) button')
+		menuUtility = self.driver.find_element_by_css_selector('.z-menu:nth-child(4) button')
 		menuUtility.click()
 
-		menuRespon = driver.find_element_by_css_selector('.z-menu-popup li:nth-child(1) a')
+		menuRespon = self.driver.find_element_by_css_selector('.z-menu-popup li:nth-child(1) a')
 		menuRespon.click()
 
-		pre.waitLoading(driver)
-
-		return driver
+		pre.waitLoading(self.driver)
 
 	def getResponses(self, tglAwal, tglAkhir, noAju):
-		driver = self.openMenu()
-
 		try:
 			checkInputTgl = EC.presence_of_element_located((By.CLASS_NAME, 'z-datebox-inp'))
-			WebDriverWait(driver, 10).until(checkInputTgl)
+			WebDriverWait(self.driver, 10).until(checkInputTgl)
 		except TimeoutException:
 			print('Timeout to load page..')
 		else:
 			print('Collect responses..')
-			inputTgl = driver.find_elements_by_css_selector('.z-datebox-inp')
+			inputTgl = self.driver.find_elements_by_css_selector('.z-datebox-inp')
 			inputTglAwal = inputTgl[0]
 			inputTglAkhir = inputTgl[1]
 
-			inputText = driver.find_elements_by_css_selector('.z-textbox')
+			inputText = self.driver.find_elements_by_css_selector('.z-textbox')
 			inputAju = inputText[1]
 
-			btnCari = driver.find_element_by_xpath('//button[contains(., "Cari")]')
+			btnCari = self.driver.find_element_by_xpath('//button[contains(., "Cari")]')
 
+			# Handling input tgl awal
+			inputTglAwal.clear()
 			inputTglAwal.click()
-			driver.execute_script('arguments[0].value = arguments[1]', inputTglAwal, tglAwal)
+			self.driver.execute_script('arguments[0].value = arguments[1]', inputTglAwal, tglAwal)
+			# Handling input tgl akhir
+			inputTglAkhir.clear()
 			inputTglAkhir.click()
-			driver.execute_script('arguments[0].value = arguments[1]', inputTglAkhir, tglAkhir)
+			self.driver.execute_script('arguments[0].value = arguments[1]', inputTglAkhir, tglAkhir)
+			# Handling input aju
+			inputAju.clear()
 			inputAju.send_keys(noAju)
 			btnCari.click()
 
 			time.sleep(1)
-			rows = driver.find_elements_by_css_selector('.z-listbox-body tbody:nth-child(2) > tr')
+			rows = self.driver.find_elements_by_css_selector('.z-listbox-body tbody:nth-child(2) > tr')
 			self.parseResponses(rows)
 			self.sortResponses()
 
@@ -70,6 +73,7 @@ class Manifest(object):
 
 	def parseResponses(self, rows):
 		print('Parse responses..')
+		self.responses = []
 		for row in rows:
 			cols = row.find_elements_by_css_selector('td > div')
 			jnResp = cols[1].get_attribute('innerHTML')
@@ -82,3 +86,17 @@ class Manifest(object):
 	
 	def sortResponses(self):
 		self.responses.sort(key = lambda x: datetime.strptime(x[3], '%d-%m-%Y %H:%M').strftime('%Y%m%d%H%M'), reverse = True)
+
+	def sendResponse(self, idButton):
+		idBtn = '#' + idButton
+		sendBtn = self.driver.find_element_by_css_selector(idBtn)
+		sendBtn.click()
+		try:
+			checkConfirmBtn = EC.presence_of_element_located((By.XPATH, '//button[contains(@class, "z-messagebox-btn") and contains(., "Yes")]'))
+			WebDriverWait(self.driver, 10).until(checkConfirmBtn)
+		except TimeoutException:
+			return [0,'Gagal mengirin respon']
+		else:
+			confirmBtn = self.driver.find_element_by_xpath('//button[contains(@class, "z-messagebox-btn") and contains(., "Yes")]')
+			confirmBtn.click()
+			return [1,'Respon telah dikirim']
--- main.py
+++ main.py
@@ -2,23 +2,49 @@ import flask
 # from bs4 import BeautifulSoup
 from datetime import datetime
 from flask import render_template, request, jsonify
+from selenium.common.exceptions import InvalidSessionIdException, TimeoutException, WebDriverException
 
 from controller.app.manifest import Manifest
 
 app = flask.Flask(__name__, template_folder='view', static_folder='assets')
 app.config["DEBUG"] = True
 
+def validate(noAju):
+	noAju = ''.join(filter(str.isdigit, noAju))
+	if len(noAju) != 26:
+		return [0, 'Isikan no aju lengkap 26 digit']
+	else:
+		return [1, noAju]
+
+def initiateManifest():
+	print('Initiate manifest..')
+
+	global driverManifest
+	driverManifest = Manifest()
+	driverManifest.openMenu()
+
 def manifest(tglAwal, tglAkhir, noAju):
+	respon = ''
 	format1 = '%Y-%m-%d'
 	format2 = '%d%m%y'
 	tglAwal = datetime.strptime(tglAwal, format1).strftime(format2)
 	tglAkhir = datetime.strptime(tglAkhir, format1).strftime(format2)
-	# return [tglAwal, tglAkhir, noAju]
 
-	manif = Manifest()
-	# respon = manif.getResponses('051219', '151219', '18052829061820191207000012')
-	respon = manif.getResponses(tglAwal, tglAkhir, noAju)
-	return respon
+	isvalid = validate(noAju)
+	if isvalid[0] == 0:
+		return ['msg', isvalid[1]]
+	else:
+		noAju = isvalid[1]
+
+	while respon == '':
+		try:
+			respon = driverManifest.getResponses(tglAwal, tglAkhir, noAju)
+		except (NameError, InvalidSessionIdException, WebDriverException):
+			if 'driverManifest' in globals():
+				driverManifest.close()
+			initiateManifest()
+
+	return ['respon', respon]
 
 @app.route('/')
 def index():
@@ -28,5 +54,10 @@ def index():
 def respon():
 	respon = manifest(request.args['tglawal'], request.args['tglakhir'], request.args['aju'])
 	return jsonify(respon)
+
+@app.route('/click', methods=['GET'])
+def click():
+	msg = driverManifest.sendResponse(request.args['id'])
+	return jsonify(msg)
 
 app.run()
