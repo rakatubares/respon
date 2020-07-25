b77b0a27f696d3f9684ba3be3a82a092d783e6ed
rakatubares <rakatubares@gmail.com>
2020-07-25 11:19:54 +0700

    change input form styles

--- respon.py
+++ respon.py
@@ -14,8 +14,8 @@ socketio = SocketIO(app)
 
 from app import route, models
 
-route.initiateManifest()
-route.initiateEkspor()
+# route.initiateManifest()
+# route.initiateEkspor()
 
 if __name__ == '__main__':
 	socketio.run(app)
